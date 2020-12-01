70cd44383bcdb0804f752886b0cd40decc99c3c8
jonen560ti <jonen560ti@gmail.com>
2020-12-01 19:08:24 +0100

    bar deselection and removal of duplicate functions.
    Bars can now be deselected by clicking the selected bar again. Removed controller functions from script.js and moved them to ControllerTest.js.

--- ControllerTest.js
+++ ControllerTest.js
@@ -1,7 +1,57 @@
 
+// function selectBar(barNo) {
+//     chosenBar = barNo;
+//     buttonState = "";
+//     show();
+// } 
+
+// function removeBar() {
+//     if (chosenBar == "") { return };
+//     barHeights.splice(chosenBar-1, 1);
+//     chosenBar = "";
+//     buttonState = "disabled"
+//     show();
+// }
+
+// function addBar() {
+// inputValue = parseInt(inputValue);
+// if (inputValue > 10 || inputValue < 1 || isNaN(inputValue)) {
+//     alert("Verdi må være mellom 1 og 10");
+//     return; }
+// let barHeight = inputValue;
+// barHeights.push(barHeight);
+// console.log(barHeights);
+// show();
+// }
+
+// function editBar() {
+//     if (validateInputValue() === false){ return };
+//     barHeights[chosenBar-1] = inputValue;
+//     console.log("Ny stolpeverdi er" + inputValue);
+
+//     show();
+// } 
+
+// function validateInputValue() {
+//     inputValue = parseInt(inputValue);
+//     if (inputValue > 10 || inputValue < 1 || isNaN(inputValue)) {
+//     alert("Verdi må være mellom 1 og 10");
+//     return false; } 
+//     else {
+//         return true;
+//     };
+// }
+
+
 function selectBar(barNo) {
+    if (chosenBar === barNo) { chosenBar = ""; 
+    alert("deselected bar")
+    buttonState = "disabled";
+    } else {
     chosenBar = barNo;
     buttonState = "";
+    }
+    
     show();
 } 
 
@@ -15,9 +65,7 @@ function removeBar() {
 
 function addBar() {
 inputValue = parseInt(inputValue);
-if (inputValue > 10 || inputValue < 1 || isNaN(inputValue)) {
-    alert("Verdi må være mellom 1 og 10");
-    return; }
+if (validateInputValue() === false){ return };
 let barHeight = inputValue;
 barHeights.push(barHeight);
 console.log(barHeights);
--- script.js
+++ script.js
@@ -53,45 +53,49 @@ function calcColor(min, max, val) {
 
 // controller
 
-function selectBar(barNo) {
-    chosenBar = barNo;
-    buttonState = "";
-    show();
-} 
+// function selectBar(barNo) {
+//     if (chosenBar === barNo) { chosenBar = ""; 
+//     alert("deselected bar")
+//     buttonState = "disabled";
+//     } else {
+//     chosenBar = barNo;
+//     buttonState = "";
+//     }
+    
+//     show();
+// } 
 
-function removeBar() {
-    if (chosenBar == "") { return };
-    barHeights.splice(chosenBar-1, 1);
-    chosenBar = "";
-    buttonState = "disabled"
-    show();
-}
+// function removeBar() {
+//     if (chosenBar == "") { return };
+//     barHeights.splice(chosenBar-1, 1);
+//     chosenBar = "";
+//     buttonState = "disabled"
+//     show();
+// }
 
-function addBar() {
-inputValue = parseInt(inputValue);
-if (inputValue > 10 || inputValue < 1 || isNaN(inputValue)) {
-    alert("Verdi må være mellom 1 og 10");
-    return; }
-let barHeight = inputValue;
-barHeights.push(barHeight);
-console.log(barHeights);
-show();
-}
+// function addBar() {
+// inputValue = parseInt(inputValue);
+// if (validateInputValue() === false){ return };
+// let barHeight = inputValue;
+// barHeights.push(barHeight);
+// console.log(barHeights);
+// show();
+// }
 
-function editBar() {
-    if (validateInputValue() === false){ return };
-    barHeights[chosenBar-1] = inputValue;
-    console.log("Ny stolpeverdi er" + inputValue);
+// function editBar() {
+//     if (validateInputValue() === false){ return };
+//     barHeights[chosenBar-1] = inputValue;
+//     console.log("Ny stolpeverdi er" + inputValue);
 
-    show();
-} 
+//     show();
+// } 
 
-function validateInputValue() {
-    inputValue = parseInt(inputValue);
-    if (inputValue > 10 || inputValue < 1 || isNaN(inputValue)) {
-    alert("Verdi må være mellom 1 og 10");
-    return false; } 
-    else {
-        return true;
-    };
-}
+// function validateInputValue() {
+//     inputValue = parseInt(inputValue);
+//     if (inputValue > 10 || inputValue < 1 || isNaN(inputValue)) {
+//     alert("Verdi må være mellom 1 og 10");
+//     return false; } 
+//     else {
+//         return true;
+//     };
+// }
